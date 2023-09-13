5ba84636eab293e7c26fb4d030821099ea7e304e
juseung <jsshin6246@gmail.com>
2023-09-13 16:13:49 +0900

    Next.js 이용한 게시판 만들기 연습

---
+++ app/util/database.js
@@ -0,0 +1,15 @@
+import { MongoClient } from 'mongodb'
+const pwd = '6U4GCrJrnkbhUJgdtt'
+const url = 'mongodb+srv://jsshin6246:'+ pwd +'@cluster0.kmyyisz.mongodb.net/'
+const options = { useNewUrlParser: true }
+let connectDB
+
+if (process.env.NODE_ENV === 'development') {
+  if (!global._mongo) {
+    global._mongo = new MongoClient(url, options).connect()
+  }
+  connectDB = global._mongo
+} else {
+  connectDB = new MongoClient(url, options).connect()
+}
+export { connectDB }
